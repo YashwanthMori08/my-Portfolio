1b053f730bd12fdf9e829efec4b5e61c1358235b
YashwanthMori08 <165082559+YashwanthMori08@users.noreply.github.com>
2024-06-20 11:15:22 +0530

    contactform.js

--- contactform.js
+++ contactform.js
@@ -1,22 +1,4 @@
-/* const username = document.getElementById('username');
-const email = document.getElementById('email');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
-form.addEventListener('submit', (e) => {
-    let messages = []
-
-    function isValidEmail(email){
-        return email.includes('@');
-    }
-
-    if (username.value === '' || username.value == null) {
-      messages.push('username is required');  
-    }
-     else if (messages.length > 0){
-    e.preventDefault()
-    errorElement.innerText = messages.join(',')
-    }
-}) */
+/* js for contact-form*/
 const submitBtn = document.getElementsByClassName('btn2');
 
 const validate = (e) => {
